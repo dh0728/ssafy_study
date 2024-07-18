e3c101f25c56386ececd06fac4844f7b4045e007
dh0728 <dustkscjswo@naver.com>
2024-07-18 23:38:58 +0900

    d

--- song_algorithm/sw_expert/1936_1대1가위바위보.py
+++ song_algorithm/sw_expert/1936_1대1가위바위보.py
@@ -1,27 +1,27 @@
-A, B = map(int, input().split())
-result = "C"
-if (A == 1 and B == 3) or (A ==2 and B == 1) or (A==3 and  B==2): # A가 이기는 경우
-    result="A"
-else:
-    result="B"
-print(result)
+# A, B = map(int, input().split())
+# result = "C"
+# if (A == 1 and B == 3) or (A ==2 and B == 1) or (A==3 and  B==2): # A가 이기는 경우
+#     result="A"
+# else:
+#     result="B"
+# print(result)
 
-#버전2 
-A,B=map(int,input().split())
-a=A-B
-if a==-1 or  a==2 : 
-    print('B')
-else:
-    print('A')
+# #버전2 
+# A,B=map(int,input().split())
+# a=A-B
+# if a==-1 or  a==2 : 
+#     print('B')
+# else:
+#     print('A')
 
-#버전3
-A,B=map(int,input().split())
-if (A-B)%3==1:
-    print('A')
-else:
-    print('B')
+# A,B=map(int,input().split())
+# if (A-B)%3==1:
+#     print('A')
+# else:
+#     print('B')
 
 #버전4
 A,B=map(int,input().split())
 print('AB'[(A-B)%3==2])
 
+print(-4//3)
