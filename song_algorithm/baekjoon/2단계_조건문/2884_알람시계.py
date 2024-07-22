329c8845c642a38001f84ff21f3884129c7a2fe3
dh0728 <dustkscjswo@naver.com>
2024-07-22 23:31:13 +0900

    문제

--- song_algorithm/baekjoon/2단계_조건문/2884_알람시계.py
+++ song_algorithm/baekjoon/2단계_조건문/2884_알람시계.py
@@ -15,9 +15,12 @@ else:
   print('0 0')
 
 # 방법2 
-total = H * 60 + M -45
-if total < 0:
-  total += 60 * 24 
-H = total // 60
-M = total % 60
+total_min = H * 60 + M -45
+
+if total_min < 0:
+  total_min += 60 * 24 
+
+H = total_min // 60
+M = total_min % 60
+
 print(H,M)
---
+++ song_algorithm/sw_expert/D1/2056_연월일달력.py
@@ -0,0 +1,17 @@
+T= int(input())
+for test_case in range(1,T+1):
+  case = input()
+  Y = case[:4]
+  M = case[4:6]
+  D = case[6:8]
+  if 1 <= int(M) <= 12:
+    if M=='02' and int(D)>28:
+      print(f'#{test_case} -1')
+    elif (M=='01' or M=='03' or M=='05' or M=='07' or M=='08' or M=='10' or M=='12') and int(D) > 31:
+      print(f'#{test_case} -1')
+    elif (M=='04' or M=='06' or M=='09' or M=='11' or M=='08' or M=='10' or M=='12') and int(D) > 30:
+      print(f'#{test_case} -1')
+    else:
+      print(f'#{test_case} {Y}/{M}/{D}')
+  else:
+    print(f'#{test_case} -1')
