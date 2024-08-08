e23a116eb23afe78eba92e3452a5dada1697737d
dustkscjswo <dustkscjswo@naver.com>
2024-08-08 17:46:52 +0900

    f

--- song_algorithm/sw_expert/D3/4837_부분집합의합.py
+++ song_algorithm/sw_expert/D3/4837_부분집합의합.py
@@ -26,3 +26,29 @@ for tc in range(1,T+1):
     visited = [0] * (12 + 1) #visited의 index가 집합이 가지는 원소
     dfs_find(1)
     print(f'#{tc} {count}')
+
+#빽트레킹 한거
+def dfs_find(v, num, sum):
+    global count
+    if v > 12: # v 원소의 개수가 12개 이상 일시
+        return
+    if num >N:  # 원소 숫자가 N개 이상 return
+        return
+    if sum >K:  # 합이 K이상일시 return
+        return
+    if num==N and sum==K:   #num이 N이거나 sum==K이면 count+1
+        count +=1           #만족하는 부분집합수 +1
+        return
+    visited[v]=1
+    dfs_find(v+1,num+1, sum+v)
+    visited[v]=0
+    dfs_find(v+1, num, sum)
+
+T=int(input())
+for tc in range(1,T+1):
+    N, K=map(int, input().split())
+    count=0     # N개의 원소를 가지고 합이 K인 부분집합의 갯수
+    visited = [0] * (12 + 1) #visited의 index가 집합이 가지는 원소
+    dfs_find(1, num=0, sum=0)
+    print(f'#{tc} {count}')
+
