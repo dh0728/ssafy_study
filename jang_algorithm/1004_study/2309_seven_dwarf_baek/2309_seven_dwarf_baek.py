553ada835be807a1d4292e30e84c2af772b5cbad
dmswjdsz <eunjung5406@naver.com>
2024-10-04 19:22:41 +0900

    1004

---
+++ jang_algorithm/1004_study/2309_seven_dwarf_baek/2309_seven_dwarf_baek.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+tall_lst = []
+for _ in range(9):
+    tall_lst.append(int(input()))
+tall_lst.sort()
+sum_t = sum(tall_lst)
+print(tall_lst)
+
+# asdf
+# for i in range(len(tall_lst)):
+#     for j in range():
